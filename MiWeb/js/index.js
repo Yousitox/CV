388375551923b6d33c8fc713871584cac17761f8
Yousitox <yousitox@protonmail.com>
2024-11-23 14:10:49 +0000

    cambios

--- MiWeb/js/index.js
+++ MiWeb/js/index.js
@@ -1,6 +1,21 @@
 console.log("Prueba111111");
 console.warn("Beta");
+alert("FACE BETA");
 
 function botonalerta() {
   alert("ENVIADO CORRECTAMENTE");
+}
+
+
+function apertura() {
+  document.getElementById("todo").style.marginLeft = "25%";
+  document.getElementById("barralateral").style.width = "25%";
+  document.getElementById("barralateral").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function cierre() {
+  document.getElementById("todo").style.marginLeft = "0%";
+  document.getElementById("barralateral").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
 }

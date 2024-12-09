6a4116d2a3d758a01e583f75ea68439988361337
Yousitox <yousitox@protonmail.com>
2024-12-09 10:30:23 +0000

    c

--- MiWeb/js/index.js
+++ MiWeb/js/index.js
@@ -2,11 +2,12 @@ console.log("Prueba111111");
 console.warn("Beta");
 alert("FACE BETA");
 
+/* Boton alerta */
 function botonalerta() {
   alert("ENVIADO CORRECTAMENTE");
 }
 
-
+/* Barra lateral */
 document.getElementById("toggleButton").addEventListener("click", function () {
   const sidebar = document.getElementById("sidebar");
   if (sidebar.style.width === "250px") {
